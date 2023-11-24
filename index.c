01a921204538d33f548f2efcc0e626157f777137
enriqueruelasgarcia <98918673+enriqueruelasgarcia@users.noreply.github.com>
2023-11-24 08:39:39 -0600

    Update index.c

--- index.c
+++ index.c
@@ -74,154 +74,294 @@ void loop(){
   delay(1000);
   lcd.clear(); // limpiamos la pantalla de caracteres
 }
+//Lee el sensor y retorna el porcentaje  
 
-//Lee el sensor y retorna el porcentaje 
-uint8_t getSoilmoisture(){
-  int a = 1023-(analogRead(sensor)); // los valores del sensor se arrojan invertidos, para mostrarlos correctamente a 1023 se le resta la lectura del sensor
-  int porcentaje = map(a,720,0,100,0); // almacenamos en la variable porcentaje el mapeo para obtener un valor entre 0 y 100 apartir de los valores maximos y minimos que arroja el sensor 
-  Serial.println(a); // imprimimos en consola la lectura del sensor
-  return porcentaje;
-}
+uint8_t getSoilmoisture(){ 
 
-//esta funcion activa el relay y por consiguiente la bomba de riego
-void RelayOn(){
-      digitalWrite(relayPin, HIGH);
-      tone(buzzerPin,  500); 
-      delay(1000);
-      noTone(buzzerPin);
-      analogWrite(pinRojo, 255);
-      analogWrite(pinVerde, 255); 
-      analogWrite(pinAzul, 0);
-}
-//esta funcion desactiva el relay y por consiguiente la bomba de riego
+  int a = 1023-(analogRead(sensor)); // los valores del sensor se arrojan invertidos, para mostrarlos correctamente a 1023 se le resta la lectura del sensor 
 
-void RelayOff(){
- digitalWrite(relayPin, LOW); 
-      noTone(buzzerPin);
-      playSongNoAut();
-      noTone(buzzerPin);
-      modoAutomatico=false;
-}
+  int porcentaje = map(a,720,0,100,0); // almacenamos en la variable porcentaje el mapeo para obtener un valor entre 0 y 100 apartir de los valores maximos y minimos que arroja el sensor  
 
-//obtiene el porcentaje y cambia el color de los leds
-void ledindicator(uint8_t data) {
-  if (data < 20) { // tonalidad roja
-    digitalWrite(pinRojo, HIGH);
-    digitalWrite(pinVerde, LOW);
-    digitalWrite(pinAzul, LOW);
-  } else if (data >= 20 && data < 55) { // tonalidad amarilla
-    digitalWrite(pinRojo, HIGH);
-    digitalWrite(pinVerde, HIGH);
-    digitalWrite(pinAzul, LOW);
-  } else if (data >= 55 && data < 75) { // tonalidad blanca
-    digitalWrite(pinRojo, HIGH);
-    digitalWrite(pinVerde, HIGH);
-    digitalWrite(pinAzul, HIGH);
-  } else if (data >= 75 && data < 90) { // tonalidad azul
-    digitalWrite(pinRojo, LOW);
-    digitalWrite(pinVerde, HIGH);
-    digitalWrite(pinAzul, LOW);
-  } else if (data >= 90 && data <= 100) { //tonalidad verde
-    digitalWrite(pinRojo, LOW);
-    digitalWrite(pinVerde, LOW);
-    digitalWrite(pinAzul, HIGH);
-  } else { //tonalidad violeta
-    digitalWrite(pinRojo, LOW);
-    digitalWrite(pinVerde, LOW);
-    digitalWrite(pinAzul, LOW);
-  }
-}
+  Serial.println(a); // imprimimos en consola la lectura del sensor 
 
-//muestra en la pantalla el porcentaje de humedad
-void showValue(uint8_t data){
-  int b= 1023-(analogRead(sensor)); // obetenemos la lectura del sensor
-  lcd.write(0); // imprime la gota de agua
-  lcd.print(data);  // imprimimos el porcentaje
-  lcd.print("%  ");
- lcd.write(1); // imprime el rayo
-  lcd.print(b); // imprimimos la lectura 
-}
+  return porcentaje; 
 
-//esta funcion reproduce la melodia cuando el modo automatico se activa
-void playSongAut() {
- tone(buzzerPin, 659.26);
-  delay(125);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 659.26);
-  delay(125);
-  noTone(buzzerPin);
-  delay(250);
-  tone(buzzerPin, 659.26);
-  delay(150);
-  noTone(buzzerPin);
-  delay(225);
-  tone(buzzerPin, 523.25);
-  delay(150);
-  noTone(buzzerPin);
-  delay(150);
-  tone(buzzerPin, 659.26);
-  delay(125);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 783.99);
-  delay(250);
-  noTone(buzzerPin);
-  delay(325);
-  tone(buzzerPin, 383.99);
-  delay(250);
-  noTone(buzzerPin);
-  delay(125);
-}
+} 
 
-//esta funcion reproduce la melodia cuando el modo automatico se desactiva
-void playSongNoAut() {
-  tone(buzzerPin, 523.25);
-  delay(300);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 783.99);
-  delay(300);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 659.26);
-  delay(300);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 880);
-  delay(200);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 987.77);
-  delay(200);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 880);
-  delay(200);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 830.61);
-  delay(200);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 932.33);
-  delay(200);
-  noTone(buzzerPin);
-  delay(125);
-  tone(buzzerPin, 830.61);
-  delay(300);
-  noTone(buzzerPin);
-  delay(125);
-   tone(buzzerPin, 659.26);
-  delay(150);
-  noTone(buzzerPin);
-  delay(125);
-   tone(buzzerPin, 587.33);
-  delay(150);
-  noTone(buzzerPin);
-  delay(125);
-   tone(buzzerPin, 659.26);
-  delay(350);
-  noTone(buzzerPin);
-  delay(125);
-}
+//esta función activa el relay y por consiguiente la bomba de riego 
+
+void RelayOn(){ 
+
+      digitalWrite(relayPin, HIGH); //Activa el relay con un valor alto en su pin de entrada 
+
+      tone(buzzerPin,  500); // sonido de advertencia del modo automático 
+
+      delay(1000); // cada segundo 
+
+      noTone(buzzerPin); // desactivamos el buzzer de este sonido de advertencia 
+
+      analogWrite(pinRojo, 255); // damos una tonalidad violeta al led RGB  
+
+      analogWrite(pinVerde, 255);  
+
+      analogWrite(pinAzul, 0); 
+
+} 
+
+//esta función desactiva el relay y por consiguiente la bomba de riego 
+
+void RelayOff(){ 
+
+digitalWrite(relayPin, LOW); //desactiva el relay con un valor bajo en su pin de entrada 
+
+      noTone(buzzerPin); //desactiva el buzzer 
+
+      playSongNoAut(); // reproduce la melodia correspondiente a cuando el modo automatico se deshabilita 
+
+      noTone(buzzerPin); // vuelve a desactivar el buzzer debido a la melodía 
+
+      modoAutomatico=false; // indicamos que el modo automático esta deshabilitado 
+
+} 
+
+//obtiene el porcentaje y cambia el color de los leds 
+
+void ledindicator(uint8_t data) { 
+
+  if (data < 20) { // tonalidad roja 
+
+    digitalWrite(pinRojo, HIGH); 
+
+    digitalWrite(pinVerde, LOW); 
+
+    digitalWrite(pinAzul, LOW); 
+
+  } else if (data >= 20 && data < 55) { // tonalidad amarilla 
+
+    digitalWrite(pinRojo, HIGH); 
+
+    digitalWrite(pinVerde, HIGH); 
+
+    digitalWrite(pinAzul, LOW); 
+
+  } else if (data >= 55 && data < 75) { // tonalidad blanca 
+
+    digitalWrite(pinRojo, HIGH); 
+
+    digitalWrite(pinVerde, HIGH); 
+
+    digitalWrite(pinAzul, HIGH); 
+
+  } else if (data >= 75 && data < 90) { // tonalidad azul 
+
+    digitalWrite(pinRojo, LOW); 
+
+    digitalWrite(pinVerde, HIGH); 
+
+    digitalWrite(pinAzul, LOW); 
+
+  } else if (data >= 90 && data <= 100) { //tonalidad verde 
+
+    digitalWrite(pinRojo, LOW); 
+
+    digitalWrite(pinVerde, LOW); 
+
+    digitalWrite(pinAzul, HIGH); 
+
+  } else { //tonalidad violeta 
+
+    digitalWrite(pinRojo, LOW); 
+
+    digitalWrite(pinVerde, LOW); 
+
+    digitalWrite(pinAzul, LOW); 
+
+  } 
+
+} 
+
+//muestra en la pantalla el porcentaje de humedad 
+
+void showValue(uint8_t data){ 
+
+  int b= 1023-(analogRead(sensor)); // obetenemos la lectura del sensor 
+
+  lcd.write(0); // imprime la gota de agua 
+
+  lcd.print(data);  // imprimimos el porcentaje 
+
+  lcd.print("%  "); 
+
+lcd.write(1); // imprime el rayo 
+
+  lcd.print(b); // imprimimos la lectura  
+
+} 
+
+  
+
+//esta funcion reproduce la melodia cuando el modo automatico se activa 
+
+void playSongAut() { 
+
+tone(buzzerPin, 659.26); 
+
+  delay(125); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 659.26); 
+
+  delay(125); 
+
+  noTone(buzzerPin); 
+
+  delay(250); 
+
+  tone(buzzerPin, 659.26); 
+
+  delay(150); 
+
+  noTone(buzzerPin); 
+
+  delay(225); 
+
+  tone(buzzerPin, 523.25); 
+
+  delay(150); 
+
+  noTone(buzzerPin); 
+
+  delay(150); 
+
+  tone(buzzerPin, 659.26); 
+
+  delay(125); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 783.99); 
+
+  delay(250); 
+
+  noTone(buzzerPin); 
+
+  delay(325); 
+
+  tone(buzzerPin, 383.99); 
+
+  delay(250); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+} 
+
+  
+
+//esta funcion reproduce la melodia cuando el modo automatico se desactiva 
+
+void playSongNoAut() { 
+
+  tone(buzzerPin, 523.25); 
+
+  delay(300); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 783.99); 
+
+  delay(300); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 659.26); 
+
+  delay(300); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 880); 
+
+  delay(200); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 987.77); 
+
+  delay(200); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 880); 
+
+  delay(200); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 830.61); 
+
+  delay(200); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 932.33); 
+
+  delay(200); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+  tone(buzzerPin, 830.61); 
+
+  delay(300); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+   tone(buzzerPin, 659.26); 
+
+  delay(150); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+   tone(buzzerPin, 587.33); 
+
+  delay(150); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+   tone(buzzerPin, 659.26); 
+
+  delay(350); 
+
+  noTone(buzzerPin); 
+
+  delay(125); 
+
+}  
